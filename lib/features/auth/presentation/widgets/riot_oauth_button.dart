f5360c7b00089d69a568bbf41621db44b894ad6d
JuanIgnacioSylvie <juansylvie@gmail.com>
2026-06-03 15:26:49 -0300

    Update default size of RiotOAuthButton from 72 to 64 for improved design consistency

--- lib/features/auth/presentation/widgets/riot_oauth_button.dart
+++ lib/features/auth/presentation/widgets/riot_oauth_button.dart
@@ -9,7 +9,7 @@ class RiotOAuthButton extends StatelessWidget {
   const RiotOAuthButton({
     super.key,
     required this.onPressed,
-    this.size = 72,
+    this.size = 64,
   });
 
   final VoidCallback? onPressed;
